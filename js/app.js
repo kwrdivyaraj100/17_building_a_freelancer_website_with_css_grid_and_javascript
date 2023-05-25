678257b90a40fed836c4acf84a7d7c3e620b18ed
Divya R <kwrdivyaraj100@gmail.com>
2023-05-25 18:01:32 +0530

    display the portfolio

--- js/app.js
+++ js/app.js
@@ -3,11 +3,27 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function loadPortfolio() {
-    const url = 'data.json';
-
-    fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data.portfolio);
+   const url = 'data.json';
+   fetch(url)
+    .then( response => response.json() )
+    .then(data =>  {
+        let html = '';
+        data.portfolio.forEach( item => {
+            // Build the Template
+            html += `   
+                <div class="item">
+                    <img src="img/${item.id}.jpg">
+                    <div class="content">
+                        <h3>${item.projectname}</h3>
+                        <p>${item.desc}</p>
+                    </div>
+                </div>
+            `;
+        });
+        // after we build the template, we add this into the HTML
+        document.querySelector('#portfolio').innerHTML = html;
     })
+    .catch(function(error) {
+        console.log(error);
+    });
 }
